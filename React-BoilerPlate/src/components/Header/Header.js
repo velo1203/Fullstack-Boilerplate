aaba71bd06d8edf2c4d0a32b056364c539e01d32
velo1203 <hokitoki1203@gmail.com>
2024-03-11 11:37:20 +0900

    first commit

---
+++ React-BoilerPlate/src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import { StyledHeader } from "../../style/layout/StyledHeader";
+
+function Header() {
+    return <StyledHeader></StyledHeader>;
+}
+
+export default Header;
---
+++ React-BoilerPlate/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import { StyledDefaultPage } from '../../style/layout/StyledDefaultPage';
+
+function Home() {
+  return (
+<StyledDefaultPage>
+
+</StyledDefaultPage>
+  );
+}
+
+export default Home;
